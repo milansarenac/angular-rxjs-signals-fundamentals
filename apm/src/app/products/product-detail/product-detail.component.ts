4cd51aacc39b5fbb55e41ae109136a58c81532d6
CT\milans <milans@comtrade.com>
2023-11-16 14:26:57 +0100

    m5 - Retrieving Data with http and Observables

--- apm/src/app/products/product-detail/product-detail.component.ts
+++ apm/src/app/products/product-detail/product-detail.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, SimpleChanges, inject } from '@angular/core';
 
 import { NgIf, NgFor, CurrencyPipe } from '@angular/common';
 import { Product } from '../product';
+import { Subscription } from 'rxjs';
+import { ProductService } from '../product.service';
 
 @Component({
     selector: 'pm-product-detail',
@@ -9,10 +11,13 @@ import { Product } from '../product';
     standalone: true,
     imports: [NgIf, NgFor, CurrencyPipe]
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnChanges, OnDestroy {
   // Just enough here for the template to compile
   @Input() productId: number = 0;
   errorMessage = '';
+  sub!: Subscription;
+
+  private productService = inject(ProductService);
 
   // Product to display
   product: Product | null = null;
@@ -20,6 +25,22 @@ export class ProductDetailComponent {
   // Set the page title
   pageTitle = this.product ? `Product Detail for: ${this.product.productName}` : 'Product Detail';
 
+  ngOnChanges(changes: SimpleChanges): void
+  {
+    const id = changes['productId'].currentValue;
+    if(id){
+      this.sub = this.productService.getProduct(id).subscribe(
+        product => this.product = product
+      )
+    }
+  };
+
+  ngOnDestroy(): void {
+    if(this.sub){
+      this.sub.unsubscribe();
+    }
+  }
+  
   addToCart(product: Product) {
   }
 }
