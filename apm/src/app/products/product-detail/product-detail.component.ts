59b6ddc65162bae0c3d3ac20dac64da1925f9498
CT\milans <milans@comtrade.com>
2023-11-16 16:41:09 +0100

    m6 - Handling Http Errors with Observables

--- apm/src/app/products/product-detail/product-detail.component.ts
+++ apm/src/app/products/product-detail/product-detail.component.ts
@@ -2,14 +2,14 @@ import { Component, Input, OnChanges, OnDestroy, SimpleChanges, inject } from '@
 
 import { NgIf, NgFor, CurrencyPipe } from '@angular/common';
 import { Product } from '../product';
-import { Subscription } from 'rxjs';
+import { EMPTY, Subscription, catchError } from 'rxjs';
 import { ProductService } from '../product.service';
 
 @Component({
-    selector: 'pm-product-detail',
-    templateUrl: './product-detail.component.html',
-    standalone: true,
-    imports: [NgIf, NgFor, CurrencyPipe]
+  selector: 'pm-product-detail',
+  templateUrl: './product-detail.component.html',
+  standalone: true,
+  imports: [NgIf, NgFor, CurrencyPipe]
 })
 export class ProductDetailComponent implements OnChanges, OnDestroy {
   // Just enough here for the template to compile
@@ -25,22 +25,29 @@ export class ProductDetailComponent implements OnChanges, OnDestroy {
   // Set the page title
   pageTitle = this.product ? `Product Detail for: ${this.product.productName}` : 'Product Detail';
 
-  ngOnChanges(changes: SimpleChanges): void
-  {
+  ngOnChanges(changes: SimpleChanges): void {
     const id = changes['productId'].currentValue;
-    if(id){
-      this.sub = this.productService.getProduct(id).subscribe(
-        product => this.product = product
-      )
+    if (id) {
+      this.sub = this.productService.getProduct(id)
+        .pipe(
+
+          catchError(err => {
+            this.errorMessage = err;
+            return EMPTY;
+          })
+        )
+        .subscribe(
+          product => this.product = product
+        )
     }
   };
 
   ngOnDestroy(): void {
-    if(this.sub){
+    if (this.sub) {
       this.sub.unsubscribe();
     }
   }
-  
+
   addToCart(product: Product) {
   }
 }
--- apm/src/app/products/product.service.ts
+++ apm/src/app/products/product.service.ts
@@ -1,7 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { catchError, Observable, of, tap, throwError } from 'rxjs';
 import { Product } from './product';
+import { ProductData } from './product-data';
+import { HttpErrorService } from '../utilities/http-error.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +12,13 @@ export class ProductService {
   private productsUrl = 'api/products';
 
   private http = inject(HttpClient);
+  private httpErrorService = inject(HttpErrorService);
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.productsUrl)
       .pipe(
-        tap(() => console.log('In http.get pipeline'))
+        tap(() => console.log('In http.get pipeline')),
+        catchError(err => this.handleError(err))
       );
   }
 
@@ -23,7 +27,13 @@ export class ProductService {
 
     return this.http.get<Product>(productUrl)
       .pipe(
-        tap(() => console.log('Get item with id: ', id))
+        tap(() => console.log('Get item with id: ', id)),
+        catchError(err => this.handleError(err))
       )
+  }
+
+  private handleError(err: HttpErrorResponse):Observable<never>{
+    const formattedMessage = this.httpErrorService.formatError(err);
+    return throwError(() => formattedMessage);
   }
 }
