08605762b0656a96b3e6f3011fc6e219166bfea5
CT\milans <milans@comtrade.com>
2023-11-17 15:08:06 +0100

    m7 - Get related data - switchMap...

--- apm/src/app/products/product.service.ts
+++ apm/src/app/products/product.service.ts
@@ -1,9 +1,11 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { catchError, Observable, of, tap, throwError } from 'rxjs';
+import { catchError, map, Observable, of, switchMap, tap, throwError } from 'rxjs';
 import { Product } from './product';
 import { ProductData } from './product-data';
 import { HttpErrorService } from '../utilities/http-error.service';
+import { ReviewService } from '../reviews/review.service';
+import { Review } from '../reviews/review';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +15,7 @@ export class ProductService {
 
   private http = inject(HttpClient);
   private httpErrorService = inject(HttpErrorService);
+  private reviewService  = inject(ReviewService);
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.productsUrl)
@@ -22,14 +25,26 @@ export class ProductService {
       );
   }
 
-  getProduct(id: number) {
+  getProduct(id: number): Observable<Product> {
     const productUrl = this.productsUrl + '/' + id;
 
     return this.http.get<Product>(productUrl)
       .pipe(
         tap(() => console.log('Get item with id: ', id)),
+        switchMap(product => this.getProductWithReviews(product)),
         catchError(err => this.handleError(err))
       )
+  }
+
+  private getProductWithReviews(product: Product): Observable<Product> {
+    if (product.hasReviews) {
+      return this.http.get<Review[]>(this.reviewService.getReviewUrl(product.id))
+        .pipe(
+          map(reviews => ({ ...product, reviews } as Product))
+        )
+    } else {
+      return of(product);
+    }
   }
 
   private handleError(err: HttpErrorResponse):Observable<never>{
